52ea784f89be16c65c04c33dde5d8534616575c5
Rayhaan Bhikha <rayhaan.bhikha@dazn.com>
2019-11-10 20:06:33 +0000

    room component added.

--- client/src/index.js
+++ client/src/index.js
@@ -1,21 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
-import Video from "./components/video/video";
+import Video from "./components/video/Video";
+import Room from "./components/room/Room";
+
 import * as serviceWorker from "./serviceWorker";
 
-const App = () => (
-  <>
-    <h1>Live stream chat</h1>
+const App = () => {
+  const [userInfo, setuserInfo] = useState();
 
-    <form>
-      Join Room:
-      <input type="text" />
-      <input type="submit" />
-    </form>
-    <Video />
-  </>
-);
+  const render = () => {
+    if (!userInfo) {
+      return <Room setuserInfo={setuserInfo} />;
+    } else {
+      return (
+        <>
+          <p>user: {userInfo.name}</p>
+          <p>room: {userInfo.room}</p>
+          <Video />
+        </>
+      );
+    }
+  };
+
+  return (
+    <>
+      <h1>Live stream chat</h1>
+      {render()}
+    </>
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
