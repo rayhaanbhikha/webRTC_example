4474bddb7f8e81079fd913d0b94dc30c82f106f2
Rayhaan Bhikha <rayhaan.bhikha@dazn.com>
2019-11-16 22:53:23 +0000

    Call  page added.

--- client/src/pages/Call/Call.jsx
+++ client/src/pages/Call/Call.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import Video from '../../components/Video/Video';
+import Controls from '../../components/Controls/Controls';
+
+import './Call.css'
 
 export default function Call(props) {
-    const userToCall = props.location.state;
+    const [video, setVideo] = useState(false);
+    const userToCall = props.location.state
+    const [stream, setStream] = useState(null);
+
+    const startVideo = async e => {
+        // await startCall(e);
+        const mediaStream = await navigator.mediaDevices.getUserMedia({ video: { width: 1280, height: 720 } });
+        document.getElementById("local-video").srcObject = mediaStream;
+        setStream(mediaStream);
+        setVideo(!video);
+    };
+
+    const stopVideo = async e => {
+        console.log(stream);
+        stream.getVideoTracks().forEach(track => track.stop());
+        document.getElementById("local-video").srcObject = null;
+        // await stopCall(e);
+        setVideo(!video);
+    };
+
     return (
         <div>
-            username {userToCall.username}
-            id {userToCall.id}
+            <div className="local-video">
+                <Video id="local-video" />
+                <Controls onStart={startVideo} onStop={stopVideo} />
+            </div>
+            {/* <button id="start-video" onClick={startVideo} disabled={video}> */}
+            {/* </button> */}
+            {/* <button id="stop-video" onClick={stopVideo} disabled={!video}> */}
+            {/* stop video */}
+            {/* </button> */}
+            {/* <Video id="remote-video" /> */}
         </div>
     )
 }
