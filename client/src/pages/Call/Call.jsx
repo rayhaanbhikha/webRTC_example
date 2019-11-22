54899649b93f58e0a62949706ab9115415c25e45
Rayhaan Bhikha <rayhaan.bhikha@dazn.com>
2019-11-22 11:13:27 +0000

    moved video css to video component

--- client/src/pages/Call/Call.jsx
+++ client/src/pages/Call/Call.jsx
@@ -10,19 +10,19 @@ export default function Call(props) {
     const [stream, setStream] = useState(null);
 
     const startVideo = async e => {
-        await startCall();
-        // const mediaStream = await navigator.mediaDevices.getUserMedia({ video: { width: { min: 1280 }, height: { min: 720 } } });
-        // document.getElementById("local-video").srcObject = mediaStream;
-        // document.getElementById("remote-video").srcObject = mediaStream;
-        // setStream(mediaStream);
+        // await startCall();
+        const mediaStream = await navigator.mediaDevices.getUserMedia({ video: { width: { min: 1280 }, height: { min: 720 } } });
+        document.getElementById("local-video").srcObject = mediaStream;
+        document.getElementById("remote-video").srcObject = mediaStream;
+        setStream(mediaStream);
         setVideo(!video);
     };
 
     const stopVideo = async () => {
-        await stopCall();
-        // stream.getVideoTracks().forEach(track => track.stop());
-        // document.getElementById("local-video").srcObject = null;
-        // document.getElementById("remote-video").srcObject = null;
+        // await stopCall();
+        stream.getVideoTracks().forEach(track => track.stop());
+        document.getElementById("local-video").srcObject = null;
+        document.getElementById("remote-video").srcObject = null;
         setVideo(!video);
     };
 
